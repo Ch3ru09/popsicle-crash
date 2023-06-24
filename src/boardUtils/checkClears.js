450756057ee5591c68c250d3c6658e0d47ab89b8
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2023-06-24 12:27:37 -0400

    made impossible to get matching colors at the start

--- src/boardUtils/checkClears.js
+++ src/boardUtils/checkClears.js
@@ -22,7 +22,7 @@ class CheckClears {
           checkRow.push(i)
 
 
-          if (row + i * 2 >= 0 && row + i * 2 < board.length) {
+          if (row + i * 2 >= 0 && row + i * 2 < board[0].length) {
             if (current == candies.getColorId(board[col][row + i * 2])) {
               checkRow.push(i * 2)
             }
@@ -31,14 +31,28 @@ class CheckClears {
       }
     }
 
-    console.log(checkCol, checkRow);
+    if (checkCol.includes(1) && checkCol.includes(-1)) {
+      return true
+    }
+    if (checkRow.includes(1) && checkRow.includes(-1)) {
+      return true
+    }
+
+    return false
   }
 
   all(board) {
+    let check = 0
     board.forEach((x, col) => {
       x.forEach((_, row) => {
-        this.checkCandy(Number(col), row, board)
+        if (this.checkCandy(col, row, board)) {
+          check++
+          board[col][row] = (board[col][row] - 1) ^ 7
+        }
       })
     });
+    if (check > 0) {
+      this.all(board)
+    }
   }
 }
