496e4bea1f3c773856e2653a8dacd249db7dcbca
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2023-06-26 14:04:18 +0800

    fixed issues with removing matching colors on spawn

--- src/boardUtils/checkClears.js
+++ src/boardUtils/checkClears.js
@@ -1,17 +1,17 @@
 class CheckClears {
   checkCandy(col, row, board) {
-    const checkCol = []
-    const checkRow = []
-    const current = candies.getColorId(board[col][row])
+    const checkCol = [];
+    const checkRow = [];
+    const current = candies.getColorId(board[col][row]);
 
     for (let i of [-1, 1]) {
       if (col + i >= 0 && col + i < board.length) {
         if (current == candies.getColorId(board[col + i][row])) {
-          checkCol.push(i)
+          checkCol.push(i);
 
           if (col + i * 2 >= 0 && col + i * 2 < board.length) {
             if (current == candies.getColorId(board[col + i * 2][row])) {
-              checkRow.push(i * 2)
+              checkRow.push(i * 2);
             }
           }
         }
@@ -19,40 +19,42 @@ class CheckClears {
 
       if (row + i >= 0 && row + i < board[0].length) {
         if (current == candies.getColorId(board[col][row + i])) {
-          checkRow.push(i)
-
+          checkRow.push(i);
 
           if (row + i * 2 >= 0 && row + i * 2 < board[0].length) {
             if (current == candies.getColorId(board[col][row + i * 2])) {
-              checkRow.push(i * 2)
+              checkRow.push(i * 2);
             }
           }
         }
       }
     }
 
-    if (checkCol.includes(1) && checkCol.includes(-1)) {
-      return true
-    }
-    if (checkRow.includes(1) && checkRow.includes(-1)) {
-      return true
-    }
-
-    return false
+    return [checkCol, checkRow];
   }
 
   all(board) {
-    let check = 0
+    console.log("loop");
+    let check = 0;
     board.forEach((x, col) => {
-      x.forEach((_, row) => {
-        if (this.checkCandy(col, row, board)) {
+      for (let row in x) {
+        let [checkCol, checkRow] = this.checkCandy(col, row, board);
+
+        if (checkCol.length < 1 && checkRow.length < 1) return;
+
+
+        if (checkCol.includes(1) && checkCol.includes(-1)) {
+          board[col][row] = (((board[col][row] + 1) % NB_CANDIES) + 1) ^ 7;
           check++
-          board[col][row] = (board[col][row] - 1) ^ 7
+
+        } else if (checkRow.includes(1) && checkRow.includes(-1)) {
+          board[col][row] = (((board[col][row] + 1) % NB_CANDIES) + 1) ^ 7;
+          check++
         }
-      })
+      };
     });
     if (check > 0) {
-      this.all(board)
+      this.all(board);
     }
   }
 }
